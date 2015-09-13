bdf631cc5e66b393daa25602674f6822203b994d
Luiz Oliveira <ziuloliveira@gmail.com>
2015-09-13 00:01:01 -0300

    hw 2 part 2 complete
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+  RATINGS = ['G','PG','PG-13','R']
+  
+  def Movie.get_all_ratings
+	RATINGS
+  end
 end
